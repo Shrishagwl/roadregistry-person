d930b46ec7d82bdee04e0bae4639b6fe8e918c9b
Nitya Mungalpara <s4057052@student.rmit.edu.au>
2025-05-28 15:27:55 +1000

    Changes in test file for a quick check

--- src/test/java/addPersonTest.java
+++ src/test/java/addPersonTest.java
@@ -9,7 +9,7 @@ public class addPersonTest {
     public void testAddPersonSuccess() {
         Person p = new Person();
  		 assertEquals(true,p.addPerson("56@_7!%&AB","32|Highland Street|Melbourne|Victoria|Australia","15-11-1990"));
-        // assertEquals(false,p.addPerson("56@&AB","32|Highland Street|Melbourne|Victoria|Australia","15-11-1990"));
+         assertEquals(true,p.addPerson("1234567890","32|Highland Street|Melbourne|Victoria|Australia","15-11-1990"));
     }
 
     // @Test
