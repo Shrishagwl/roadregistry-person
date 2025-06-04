b5832019d6472d5f4180d32d943f69c578fd2de3
Nitya Mungalpara <s4057052@student.rmit.edu.au>
2025-06-05 09:47:53 +1000

    Changed the names of unit tests

--- src/test/java/addPersonTest.java
+++ src/test/java/addPersonTest.java
@@ -7,32 +7,32 @@ import java.time.format.DateTimeFormatter;
 public class addPersonTest {
 
     @Test
-    public void testPersonIDSuccess() {
+    public void testCorrectPersonID() {
         Person p = new Person("56@_7!%&AB","Rohan","Patel","32|Highland Street|Melbourne|Victoria|Australia","01-01-2090",false);
  		assertEquals(true,p.addPerson());
     }
 
     @Test
-    public void testPersonIDFailure() {
+    public void testIncorrectPersonID() {
         Person p = new Person("12345678","Ram","Lakhan","3|Lonsdale Street|Melbourne|Victoria|Australia","05-03-2000",false);
  		assertFalse(p.addPerson());
 
     }
 
     @Test
-    public void testAddressFailure() {
+    public void testIncorrectAddress() {
         Person p = new Person("59@o%&ZB","Raj","Shah","12|McKanzie Street|Sydney|New South Wales|Australia","12-09-1990",false);
  		assertFalse(p.addPerson());
     }
 
     @Test
-    public void testAddressSuccess() {
+    public void testCorrectAddress() {
         Person p = new Person("51@)8*%&PT","Ramesh","Dadhaniya","12|McKanzie Street|Sydney|Victoria|Australia","12-09-1990",false);
  		assertFalse(p.addPerson());
     }
 
     @Test
-    public void testDate() {
+    public void testInvalidDateFormat() {
         Person p = new Person("3351yt%&PT","Ramesh","Dadhaniya","12|McKanzie Street|Sydney|Victoria|Australia","5-08-1990",false);
  		assertEquals("Invalid date format. Use DD-MM-YYYY.",p.addPerson());
     }
